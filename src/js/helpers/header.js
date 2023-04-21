4d913794842fe7567e25d113dbd86229cace7310
Albert <kamalov00albert@gmail.com>
2023-04-21 17:32:17 +0300

    Подготовил разметку для страницы events

--- src/js/helpers/header.js
+++ src/js/helpers/header.js
@@ -1,6 +1,7 @@
 export default function header() {
   const header = document.querySelector('.header');
   const pageWrapper = document.querySelector('.page-wrapper');
+  const intro = document.querySelector('.js-intro-block');
 
   /*const burger = document.querySelector('.header__burger');
   const mobileNavigation = document.querySelector('.header__mobile')
@@ -11,7 +12,12 @@ export default function header() {
     header.classList.toggle('active');
   })*/
 
-  pageWrapper.style.setProperty('--header-height', header.clientHeight + 'px')
+
+  if (intro) {
+    intro.style.setProperty('--header-height', header.clientHeight + 'px');
+  } else {
+    pageWrapper.style.setProperty('--header-height', header.clientHeight + 'px');
+  }
 
   window.addEventListener('scroll', () => {
     if (header.getBoundingClientRect().top > document.documentElement.getBoundingClientRect().top) {
